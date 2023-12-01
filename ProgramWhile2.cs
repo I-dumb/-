51a93bae55da2fa8a7015ec51be0a1fd73436a2b
I-dumb <152587050+I-dumb@users.noreply.github.com>
2023-12-01 11:36:43 +0500

    Возврощает сумму чисел пока user не введёт 0

---
+++ ProgramWhile2.cs
@@ -0,0 +1,13 @@
+﻿int Poka0()
+{
+    int ans = 0;
+    int slogaemoe = 0;
+    do
+    {
+        slogaemoe = Convert.ToInt32(Console.ReadLine());
+        ans = ans + slogaemoe;
+    }
+    while (slogaemoe != 0);
+    return ans;
+}
+Console.WriteLine(Poka0());
