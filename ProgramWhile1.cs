c53ba0c6b15ab299882842f041fb092124bdcac9
I-dumb <152587050+I-dumb@users.noreply.github.com>
2023-12-01 11:18:49 +0500

    Программа считающая числа от x до y

---
+++ ProgramWhile1.cs
@@ -0,0 +1,17 @@
+﻿int Sum(int x, int y)
+{
+    if(x>y)
+    {
+        return 0;
+    }
+    int ans = x;
+    while (x < y)
+    {
+        x++;
+        ans = ans + x;
+
+    }
+    return ans;
+
+}
+Console.WriteLine(Sum(10, 9));
