8436047ead77bfeccda9469ecf9d0cb7fe19950f
I-dumb <152587050+I-dumb@users.noreply.github.com>
2023-12-01 11:43:15 +0500

    Считает введённые числа пока их сумма<100

---
+++ ProgramWhile3.cs
@@ -0,0 +1,15 @@
+﻿int Poka100()
+{
+    int ans = 0;
+    int slogaemoe = 0;
+    int kolvo = 0;
+    do
+    {
+        slogaemoe = Convert.ToInt32(Console.ReadLine());
+        ans = ans + slogaemoe;
+        kolvo++;
+    }
+    while (ans < 100);
+    return kolvo;
+}
+Console.WriteLine(Poka100());
