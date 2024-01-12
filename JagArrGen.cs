56de8be2bffa46572526495d4dd99081e6c780a5
I-dumb <152587050+I-dumb@users.noreply.github.com>
2024-01-12 11:33:08 +0500

    Метод для генерации массива

---
+++ JagArrGen.cs
@@ -0,0 +1,35 @@
+﻿int[][] JagArrGen()
+{
+    Console.WriteLine("Введите кол-во строк в массиве");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    int[][] JagArr = new int[rows][];
+
+    for(int i = 0; i<JagArr.Length;i++)
+    {
+        Console.WriteLine($"Введите кол-во элементов для строки {i+1}");
+        JagArr[i] = new int[Convert.ToInt32(Console.ReadLine())];
+    }
+
+    for (int i = 0; i<JagArr.Length; i++)
+    {
+        for(int j = 0; j < JagArr[i].Length; j++)
+        {
+            Console.WriteLine($"Введите элемент № {j+1} в строке {i+1}");
+            JagArr[i][j] = Convert.ToInt32(Console.ReadLine());
+        }
+
+    }
+   return JagArr;
+}
+
+int[][] arr = JagArrGen();
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine($"\nэлементы в строке {i}:");
+    for (int j = 0; j < arr[i].Length; j++)
+    {
+        Console.Write(arr[i][j] + "\t");
+    }
+
+}
